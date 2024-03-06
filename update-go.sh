73d03852f97c7ac1925042b63f7e38fdd07fafc2
John Ingram <john.ingram@edeltech.ch>
2024-03-06 22:37:59 +0100

    Add go update command

---
+++ update-go.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GO_URL="https://go.dev/dl"
+GO_FILE="go1.22.1.linux-amd64.tar.gz"
+
+cd /tmp
+curl -LO --progress-bar ${GO_URL}/${GO_FILE}
+tar -zxf ${GO_FILE} -C /usr/local
+rm -f ${GO_FILE}
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/GO
+export PATH=$PATH:$GOPATH/bin
+
+go version
