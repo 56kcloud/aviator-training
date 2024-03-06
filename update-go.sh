1e5e32f3fc51597877c857721cc1372622af1408
John Ingram <john.ingram@edeltech.ch>
2024-03-06 22:47:53 +0100

    Update update-go.sh

--- update-go.sh
+++ update-go.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+sudo yum remove golang -y
+
 GO_URL="https://go.dev/dl"
 GO_FILE="go1.22.1.linux-amd64.tar.gz"
 
-cd /tmp
+pushd /tmp &> /dev/null
 curl -LO --progress-bar ${GO_URL}/${GO_FILE}
 sudo tar -zxf ${GO_FILE} -C /usr/local
 sudo rm -f ${GO_FILE}
+popd &> /dev/null
 
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/GO
