0bd3b691a5f17c003cc399f7e3f112544bee7560
John Ingram <john.ingram@edeltech.ch>
2024-03-06 22:39:19 +0100

    Update update-go.sh

--- update-go.sh
+++ update-go.sh
@@ -4,8 +4,8 @@ GO_FILE="go1.22.1.linux-amd64.tar.gz"
 
 cd /tmp
 curl -LO --progress-bar ${GO_URL}/${GO_FILE}
-tar -zxf ${GO_FILE} -C /usr/local
-rm -f ${GO_FILE}
+sudo tar -zxf ${GO_FILE} -C /usr/local
+sudo rm -f ${GO_FILE}
 
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/GO
